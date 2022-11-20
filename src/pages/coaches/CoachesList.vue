15bb64277f8670718cbbff613b5127dac5177594
mankaa <mankaache10@gmail.com>
2022-11-20 06:21:14 +0100

    feat:added the register as a coach feature

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,7 @@
 <base-card>
     <div class="controls">
          <base-button mode="outline"> Refresh</base-button>
-        <base-button link to='/register'> Register as coach</base-button>
+        <base-button v-if="!isCoach" link to='/register'> Register as coach</base-button>
     </div>
     <ul v-if="hasCoaches">
         <coach-item v-for="coach in filteredCoaches" 
@@ -41,6 +41,11 @@ import CoachFilter from "../../components/coaches/CoachFilter.vue"
             }
         },
         computed:{
+            isCoach(){
+
+            return this.$store.getters['coaches/isCoach']
+            },
+    
             filteredCoaches(){
                 const coaches =  this.$store.getters['coaches/coaches'] //[namespaced name/real getter]
                 return coaches.filter(coach => {
--- src/pages/coaches/CoachesRegistration.vue
+++ src/pages/coaches/CoachesRegistration.vue
@@ -1,13 +1,28 @@
 <template>
     <section>
+        <base-card>
         <h2>Register as a coach now!</h2>
-        
+        <coach-form @save-data="formRegistration"></coach-form>
+        </base-card>
+    
     </section>
 </template>
 
 <script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
     export default {
-        
+        components:{
+            CoachForm
+        },
+     
+        methods: {
+            formRegistration(data){
+                this.$store.dispatch('coaches/registerCoach',data)
+                // this.$router.push('/coaches') //or
+                this.$router.replace('/coaches')
+            }
+        },
+
     }
 </script>
 
